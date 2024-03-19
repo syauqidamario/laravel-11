2861356e0f6a2952a1a8957ab574eb999f07ad25
syauqidamario <syauqi.djohan@student.umn.ac.id>
2024-03-19 19:03:32 +0700

    Exceptions in PHP11 & default database

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,8 @@ use App\Http\Middleware\LogMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -16,5 +18,9 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->append(LogMiddleware::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->dontReport(\App\Exceptions\ValidationError::class);
+
+        $exceptions->renderable(function (\App\Exceptions\ValidationError $exception, Request $request) {
+            return response("Bad Request", 400);
+        });
     })->create();
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/validation', function () {
+    throw new \App\Exceptions\ValidationError("Invalid input");
+});
